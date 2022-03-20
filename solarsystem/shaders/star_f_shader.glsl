0d6fbe5458d2ada459fa1c3c935307417b407671
lyjustinn <87103438+lyjustinn@users.noreply.github.com>
2022-03-20 15:42:51 -0400

    Update star shader to use texture

--- solarsystem/shaders/star_f_shader.glsl
+++ solarsystem/shaders/star_f_shader.glsl
@@ -9,9 +9,9 @@ uniform sampler2D u_texture;
 void main()
 {
     //vec3 colour = texture(u_texture, o_pos).rgb;
-    FragColor = vec4(1.0f, 0.73f, 0.03f, 1.0f) * 2.0;
+    //FragColor = vec4(1.0f, 0.73f, 0.03f, 1.0f) * 2.0;
     vec2 coord = vec2(o_pos * 0.5 + vec3(0.5));
-    //FragColor = texture(u_texture, coord);
+    FragColor = texture(u_texture, coord);
 
     BrightColor = vec4(FragColor.rgb, 1.0);
 } 
